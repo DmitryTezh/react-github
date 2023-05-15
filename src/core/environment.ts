bc5218aee8f07b1597f78e520757b7ff8e07f19c
DIMITRY <dmitry.tezhelnikov@gmail.com>
2023-05-15 22:39:37 +0300

    bug fixes

--- src/core/environment.ts
+++ src/core/environment.ts
@@ -1,9 +1,12 @@
 import { Octokit } from 'octokit';
+import { once } from '../utils';
 
 export interface Environment {
     remoteApi: InstanceType<typeof Octokit>;
 }
 
+const createRemoteApi = once(() => new Octokit({}));
+
 export const createEnvironment = (): Environment => ({
-    remoteApi: new Octokit({}),
+    remoteApi: createRemoteApi(),
 });
